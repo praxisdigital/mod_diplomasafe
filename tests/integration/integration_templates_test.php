6a59fe0db094b86ea176ed208df686e524c1bd28
Johnny Drud <jdr@praxis.dk>
2021-01-22 15:32:02 +0100

    Remove unnecessary code and add more tests

---
+++ tests/integration/integration_templates_test.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @developer   Johnny Drud
+ * @date        22-01-2021
+ * @company     https://diplomasafe.com
+ * @copyright   2021 Diplomasafe ApS
+ */
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class
+ */
+class integration_templates_testcase extends advanced_testcase
+{
+    /**
+     * @test
+     */
+    public function can_add_template() {
+        $this->resetAfterTest();
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function can_store_template() {
+        $this->resetAfterTest();
+
+
+    }
+}
