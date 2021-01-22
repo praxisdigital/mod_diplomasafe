6c499fec90341e31bc69dd08c1349d1087b72af6
Johnny Drud <jdr@praxis.dk>
2021-01-22 12:27:29 +0100

    Make sure we are only adding diplomas to the queue if a valid template exists for the course (a course module exists and a template is selected)

--- classes/observer.php
+++ classes/observer.php
@@ -10,6 +10,7 @@ namespace mod_diplomasafe;
 
 use core\event\course_completed;
 use mod_diplomasafe\entities\queue_item;
+use mod_diplomasafe\factories\template_factory;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -24,18 +25,24 @@ class observer
      * @param course_completed $course_completed
      *
      * @throws \dml_exception
+     * @throws \coding_exception
      */
     public static function course_completed(course_completed $course_completed) : void {
-        if (!empty($course_completed->courseid) && !empty($course_completed->relateduserid)) {
-            $now = time();
-            $queue = new queue();
-            $queue->push(new queue_item([
-                'course_id' => $course_completed->courseid,
-                'user_id' => $course_completed->userid,
-                'status' => queue_item::QUEUE_ITEM_STATUS_PENDING,
-                'time_created' => $now,
-                'time_modified' => $now,
-            ]));
+        if (!empty($course_completed->courseid)
+            && !empty($course_completed->relateduserid)) {
+            try {
+                $template_repository = template_factory::get_repository();
+                $template_repository->get_by_course_id($course_completed->courseid);
+                $now = time();
+                $queue = new queue();
+                $queue->push(new queue_item([
+                    'course_id' => $course_completed->courseid,
+                    'user_id' => $course_completed->userid,
+                    'status' => queue_item::QUEUE_ITEM_STATUS_PENDING,
+                    'time_created' => $now,
+                    'time_modified' => $now,
+                ]));
+            } catch (\RuntimeException $e) {}
         }
     }
 }
